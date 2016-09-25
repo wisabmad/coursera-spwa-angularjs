2dcde7dde4e0ab8a43ac1645d5a641eeab06827e
Wissam A.A <wissam.abu_ahmad@e4company.com>
2016-09-25 22:35:43 +0200

    completing the minimal requirements of the assignment by adding the found items components as well as the html template file

--- mod3-sol/founditems.component.js
+++ mod3-sol/founditems.component.js
@@ -2,4 +2,30 @@
 'use strict';
 
 angular.module('NarrowItDownApp')
-.component('foundItems', {});
+.component('foundItems', {
+  templateUrl: 'foundItems.template.html',
+  controller: FoundItemsComponentController,
+  bindings: {
+    foundItems: '<',
+    onRemove: '&'
+  }
+});
+
+function FoundItemsComponentController() {
+  var $ctrl = this;
+
+  $ctrl.remove = function (myIndex) {
+    $ctrl.onRemove({ index: myIndex });
+  };
+  $ctrl.isEmpty = function () {
+    if ($ctrl.foundItems == undefined) {
+      return false;
+    }
+    else if ($ctrl.foundItems.length == 0) {
+      return true;
+
+    }
+  }
+}
+
+})();
--- mod3-sol/narrow.controller.js
+++ mod3-sol/narrow.controller.js
@@ -7,17 +7,24 @@ angular.module('NarrowItDownApp')
 NarrowItDownController.inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService) {
   var narrower = this;
-  // narrower.searchTerm = "";
   narrower.narrowIt = function() {
     console.log('button was clicked. searchTerm is '+ narrower.searchTerm);
-    var foundItemsPromise = MenuSearchService.getMatchedMenuItems(narrower.searchTerm);
-    foundItemsPromise.then(function(response) {
-      // console.log("back to narrow.controller, foundItemPromise is ", foundItemsPromise);
-      console.log("back to narrow.controller, foundItemPromise is ", foundItemsPromise.$$state.value); // is there a better way to access this data?? maybe by encapsulating it differently at the MenuSearchService side??
-
-    })
+    if (narrower.searchTerm == undefined || narrower.searchTerm.trim() == "") {
+      narrower.foundItems = [];
+    }
+    else {
+      var foundItemsPromise = MenuSearchService.getMatchedMenuItems(narrower.searchTerm);
+      foundItemsPromise.then(function(response) {
+        // console.log("back to narrow.controller, foundItemPromise is ", foundItemsPromise);
+        console.log("back to narrow.controller, foundItemPromise is ", foundItemsPromise.$$state.value); // is there a better way to access this data?? maybe by encapsulating it differently at the MenuSearchService side??
+        narrower.foundItems = foundItemsPromise.$$state.value;
+      })
+    }
   }
 
+  narrower.removeItem = function (itemIndex) {
+    narrower.foundItems.splice(itemIndex,1);
+  };
 
 }
 
